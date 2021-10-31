3e5eefb1f74f794a212ece867ad2415fa327e6f2
Alexsander Alves <alvesalexsander@live.com>
2021-10-31 17:49:53 -0300

    elastic search

---
+++ elastic.js
@@ -0,0 +1,7 @@
+
+const apm = require('elastic-apm-node').start({
+  serviceName: '',
+  secretToken: 'kVveUc2mG9cZLkyV1V',
+  serverUrl: 'https://726e567efacf492ca1aaf3958b7e8980.apm.us-east1.gcp.elastic-cloud.com:443',
+  environment: 'production'
+});
