70cb0276f6ab10cc6db11658ce9a9532a1d07d24
Alexsander Alves <alvesalexsander@live.com>
2021-10-30 21:56:14 -0300

    npm engines e renomeira app_port para port

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const morgan = require('morgan');
 
 const app = express();
 
-const port = process.env.APP_PORT || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(helmet());
 app.use(express.json());
